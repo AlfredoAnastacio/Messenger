46055c0973c05bea69a56d58b416f1ae28a7810d
AlfredoAnastacio <xbox.07@hotmail.com>
2018-11-21 13:49:31 -0600

    Incluido BV en plantilla de login form

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,6 +9,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import BootstrapVue from 'bootstrap-vue' //importación de Boostrap-Vue
+Vue.use(BootstrapVue); //uso de Boostrap-Vue
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
